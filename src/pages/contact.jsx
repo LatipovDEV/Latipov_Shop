f51ba8f7a443ce04ad55c75df8cdc3e99a05d063
Olmosjon <latipovdev@gmail.com>
2023-07-26 12:27:43 +0500

    first commit

---
+++ src/pages/contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import '../App.css'
+
+const Contact = () => {
+  return (
+    <div className='notfound'>
+      NotFound 404
+      <Link to='/' className='home'>Home</Link>
+    </div>
+  )
+}
+
+export default Contact
